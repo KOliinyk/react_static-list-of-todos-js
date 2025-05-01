ee2951ae5830b49e7ce12b4dace99c8a6b63ec32
Kostya Oliinyk <aquila0905@gmail.com>
2025-05-01 19:26:24 +0300

    add task solution

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -4,7 +4,7 @@ import { UserInfo } from '../UserInfo'; // Шлях до компонента Us
 export const TodoInfo = ({ todo }) => {
   return (
     <div className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`}>
-      <h3>{todo.title}</h3>
+      <h3 className="TodoInfo__title">{todo.title}</h3>
       {todo.user && <UserInfo user={todo.user} />}
     </div>
   );
--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,14 +1,5 @@
 import React from 'react';
 
 export const UserInfo = ({ user }) => {
-  return (
-    <div className="UserInfo">
-      <p>
-        <strong>Name:</strong> {user.name}
-      </p>
-      <p>
-        <strong>Email:</strong> {user.email}
-      </p>
-    </div>
-  );
+  return <div className="UserInfo">{user.name}</div>;
 };
